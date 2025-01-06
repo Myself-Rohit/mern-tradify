4f089d29cf3deac00d5ca15ecc4db91b42126b03
rohit negi <rohitnegi263139@gmail.com>
2025-01-06 12:29:43 +0530

    create get all company, get single company and update stock routes

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -35,3 +35,53 @@ export const registerCompany = async (req, res, next) => {
 		return next(errorHandler(500, "Internal server error"));
 	}
 };
+
+export const getCompanies = async (req, res, next) => {
+	try {
+		const allCompany = await Company.find({});
+		if (!allCompany) {
+			return next(errorHandler(400, "No company found"));
+		}
+		return res.status(200).json(allCompany);
+	} catch (error) {
+		next(errorHandler(500, "Internal server error"));
+	}
+};
+
+export const getCompany = async (req, res, next) => {
+	try {
+		const company = await Company.findOne({ _id: req.params.id });
+		if (!company) {
+			return next(errorHandler(400, "company not found"));
+		}
+		return res.status(200).json(company);
+	} catch (error) {
+		next(errorHandler(500, "Internal server error"));
+	}
+};
+
+export const updateStockPrice = async (req, res, next) => {
+	try {
+		const { companyId, transactionType, shares } = req.query;
+		if (!companyId || !transactionType || !shares) {
+			return next(errorHandler(404, "Not found"));
+		}
+		const company = await Company.findById(companyId);
+		// Define price impact factor
+		const impactFactor = 0.001; // Adjust this for stronger or weaker impact
+
+		if (transactionType === "buy") {
+			company.availableShares -= shares;
+			company.stockPrice += shares * impactFactor; // Price increases with buys
+		} else if (transactionType === "sell") {
+			company.availableShares -= shares;
+			company.stockPrice -= shares * impactFactor; // Price decreases with sells
+		}
+
+		company.stockPrice = Math.max(1, company.stockPrice); // Ensure price doesn't go below 1
+		await company.save();
+		return res.status(200).json(company);
+	} catch (error) {
+		next(errorHandler(500, "Internal server error"));
+	}
+};
--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { registerCompany } from "../controllers/company.controller.js";
+import {
+	registerCompany,
+	getCompanies,
+	getCompany,
+	updateStockPrice,
+} from "../controllers/company.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.post("/register", verifyToken, registerCompany);
+router.get("/all", verifyToken, getCompanies);
+router.get("/:id", verifyToken, getCompany);
+router.put("/update", verifyToken, updateStockPrice);
 
 export default router;
